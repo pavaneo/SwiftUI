296f14048348879e15bd3aaeec17da86624c0681
Pavan Jain <pavan.kumarj@tyfone.com>
2023-05-06 09:40:42 +0530

    SwiftUI Project Created

---
+++ SwiftUI-Learning/CustomView/IconsView.swift
@@ -0,0 +1,20 @@
+//
+//  IconsView.swift
+//  SwiftUI-Learning
+//
+//  Created by Pavan Kumar J on 06/05/23.
+//
+
+import SwiftUI
+
+struct IconsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IconsView_Previews: PreviewProvider {
+    static var previews: some View {
+        IconsView()
+    }
+}
